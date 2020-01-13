ae2f669eadfb992af7491b6b630a13a4e195f4d5
Wilk <wilk3ert@gmail.com>
2020-01-14 00:18:15 +0100

    first example of morse code

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -13,13 +13,121 @@ import {
   IonListHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonButton,
+  IonInput
 } from '@ionic/react';
 import { book, build, colorFill, grid } from 'ionicons/icons';
-import React from 'react';
+import React, { useState } from 'react';
 import './Tab1.css';
 
+const alphabet: { [key: string]: string } = {
+  a: '.-',
+  b: '-...',
+  c: '-.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..',
+  1: '.----',
+  2: '..---',
+  3: '...--',
+  4: '....-',
+  5: '.....',
+  6: '-....',
+  7: '--...',
+  8: '---..',
+  9: '----.',
+  0: '-----',
+};
+
+/*
+hello world
+
+hello
+. !. . !. . !. .
+!...
+.
+!...
+.-..
+!...
+.-..
+!...
+---
+
+!.....
+
+world
+
+
+*/
+
+const POINT_DELAY_MS = 250;
+const DASH_DELAY_MS = 750;
+const SPACE_DELAY_MS = 500;
+const delay = (ms: number): Promise<void> => new Promise(r => setTimeout(r, ms));
+// @ts-ignore
+const on = () => new Promise((resolve, reject) => window.plugins.flashlight.switchOn(resolve, reject));
+// @ts-ignore
+const off = () => new Promise((resolve, reject) => window.plugins.flashlight.switchOff(resolve, reject));
+
 const Tab1: React.FC = () => {
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  // char: 'd'
+  const morse = async (char: string): Promise<void> => {
+    // sequence: d -> '-..'
+    const sequence: string = alphabet[char];
+    for (let i = 0; i < sequence.length; i++) {
+      // next: '-'
+      const next = sequence[i];
+      await on();
+      await delay(next === '.' ? POINT_DELAY_MS : DASH_DELAY_MS);
+      await off();
+      await delay(POINT_DELAY_MS);
+    }
+  };
+
+  const translate = async (input: string): Promise<void> => {
+    setLoading(true);
+
+    // input: 'hello world'
+    const words = input.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      // words[i]: 'hello'
+      const word = words[i];
+      for (let j = 0; j < word.length; i++) {
+        // word[i]: 'h'
+        await morse(word[i]);
+        await delay(SPACE_DELAY_MS);
+      }
+
+      await delay(SPACE_DELAY_MS);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -35,10 +143,12 @@ const Tab1: React.FC = () => {
             <IonCardTitle>Welcome to Ionic</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
+            <IonInput onInput={event => {
+              // @ts-ignore
+              setText(event.target.value)
+            }
+            } />
+            <IonButton disabled={loading} onClick={() => translate(text)}>Translate</IonButton>
           </IonCardContent>
         </IonCard>
 
